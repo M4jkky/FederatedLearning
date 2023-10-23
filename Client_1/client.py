af3d66970ee186caf22d4f9750fd926cec97691a
m4jkky <marian.hrin@student.tuke.sk>
2023-10-23 10:55:36 +0200

    ahoj

--- Client_1/client.py
+++ Client_1/client.py
@@ -39,6 +39,7 @@ class Client(fl.client.NumPyClient):
         with open('./conf/config.yaml', 'r') as config_file:
             config = yaml.safe_load(config_file)
         print("Received config:", config)
+        print("avoj")
         self.set_parameters(parameters)
         optimizer = torch.optim.Adam(self.model.parameters(), lr=config['learning_rate'])
         train(self.model, self.train_loader, optimizer, num_epochs=config['local_epochs'], device=self.device)
